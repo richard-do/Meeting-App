ebaba06d571a021d72c760d6a02a869e40487d61
richard-do <richard-do@users.noreply.github.com>
2020-09-25 16:08:54 -0400

    added todo, fixed spelling error in code

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(port, ()=> {
-    console.log(`Server is running on poirt: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
