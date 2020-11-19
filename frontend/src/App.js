17c5a86f19a7150c70a00537c96ef8fbcd6d2bae
richard-do <richard-do@users.noreply.github.com>
2020-11-18 23:22:47 -0500

    Added bootstrap, prepare imports and empty files accordingly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 
+
+import Navbar from "./components/navbar.component";
+import Map from "./components/map.component";
+import CreateMarker from "./components/create-user.component";
+import CreateUser from "./components/create-user.component";
+
+// currently only accomodates one map (current)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <br/>
+      <Route path="/" exact component={Map} />
+      <Route path="/edit/:id" component={EditMap} />
+      <Route path="/create" component={CreateMarker} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
