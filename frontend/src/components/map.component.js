9279bb143a34b3e27665d31627904699c8d39b74
richard-do <richard-do@users.noreply.github.com>
2020-12-25 20:53:40 -0500

    Used google-map-react to generate map
    Uses npm install --save google-map-react and the sample provided on npmjs to generate a basic map.  API key is necessary in order for this to work properly.

--- frontend/src/components/map.component.js
+++ frontend/src/components/map.component.js
@@ -1,11 +1,35 @@
-import React, {Component} from 'react';
-
-export default class Map extends Component{
-    render(){
-        return (
-            <div>
-                <p>In map component.</p>
-            </div>
-        )
-    }
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 43.5890,
+      lng: 79.6441
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* API KEY */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={43.5890}
+            lng={79.6441}
+            text="My Marker (Mississauga)"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
+
+export default Map;
