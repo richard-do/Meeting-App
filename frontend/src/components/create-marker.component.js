039751f29b1c8e861ea5e8333d934c27a3f1e9c4
richard-do <richard-do@users.noreply.github.com>
2020-12-27 19:13:53 -0500

    Update create-marker.component.js

--- frontend/src/components/create-marker.component.js
+++ frontend/src/components/create-marker.component.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 export default class CreateMarker extends Component{
 
